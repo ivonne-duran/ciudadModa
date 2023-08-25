3427b5f1b8721b26214733c63a7f72b33a43733a
lorena <loremedi0103@gmail.com>
2023-08-25 00:17:02 -0600

    guardando nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Texto from './componentes/Texto/Texto';
 import Numero from './componentes/Numero/Numero';
+import Navbar from './componentes/Navbar/Navbar';
 import { Container } from 'reactstrap';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Container>
         { /* Este es solo un ejemplo de como usar los controles */ }
+        <Navbar/>
         <Texto texto={"Nombre Articulo"} mensaje={"Digite el nombre del articulo"} nombre={"nombreArticulo"} />
         <Numero texto={"Valor del Articulo"} mensaje={"Valor del articulo"} nombre={"valorArticulo"} />
         <Texto texto={"Descripción del articulo"} mensaje={"Agregue una descripcion del articulo"} nombre={"descripcionArticulo"} />
